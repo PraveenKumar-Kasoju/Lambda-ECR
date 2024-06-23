aaef16c2f4f8f284db8bbbfdce11c1cbb7f2d015
PraveenKasoju <praveenkasoju20@gmail.com>
2024-06-23 11:15:29 +0530

    justupdatedcodegf

--- pipeline.py
+++ pipeline.py
@@ -7,98 +7,110 @@ lambda_function_name = 'github-to-lambda-demo'
 github_owner = 'PraveenKumar-Kasoju'
 github_repo = 'Lambda-Ecr'
 github_branch = 'main'
-github_token = 'github-token'
+github_token = 'your-github-token'
 codebuild_project_name = 'github-lambda-ecr'
 artifact_store_s3_bucket = 'lambda-deploy-bucket'
 codepipeline_role_arn = 'arn:aws:iam::506236563550:role/github-lambda-ecr-role'
 
-codepipeline_client = boto3.client('codepipeline', region_name=us-east-1)
+# Create the CodePipeline client
+codepipeline_client = boto3.client('codepipeline', region_name=region)
 
-response = codepipeline_client.create_pipeline(
-    pipeline={
-        'name': pipeline_name,
-        'roleArn': codepipeline_role_arn,
-        'artifactStore': {
-            'type': 'S3',
-            'location': artifact_store_s3_bucket
+# Define the pipeline structure
+pipeline = {
+    'name': pipeline_name,
+    'roleArn': codepipeline_role_arn,
+    'artifactStore': {
+        'type': 'S3',
+        'location': artifact_store_s3_bucket
+    },
+    'stages': [
+        {
+            'name': 'Source',
+            'actions': [
+                {
+                    'name': 'SourceAction',
+                    'actionTypeId': {
+                        'category': 'Source',
+                        'owner': 'ThirdParty',
+                        'provider': 'GitHub',
+                        'version': '1'
+                    },
+                    'outputArtifacts': [
+                        {
+                            'name': 'SourceOutput'
+                        }
+                    ],
+                    'configuration': {
+                        'Owner': github_owner,
+                        'Repo': github_repo,
+                        'Branch': github_branch,
+                        'OAuthToken': github_token
+                    },
+                    'runOrder': 1
+                }
+            ]
         },
-        'stages': [
-            {
-                'name': 'Source',
-                'actions': [
-                    {
-                        'name': 'SourceAction',
-                        'actionTypeId': {
-                            'category': 'Source',
-                            'owner': 'ThirdParty',
-                            'provider': 'GitHub',
-                            'version': '1'
-                        },
-                        'outputArtifacts': [
-                            {
-                                'name': 'SourceOutput'
-                            }
-                        ],
-                        'configuration': {
-                            'Owner': github_owner,
-                            'Repo': github_repo,
-                            'Branch': github_branch,
-                            'OAuthToken': github_token
+        {
+            'name': 'Build',
+            'actions': [
+                {
+                    'name': 'BuildAction',
+                    'actionTypeId': {
+                        'category': 'Build',
+                        'owner': 'AWS',
+                        'provider': 'CodeBuild',
+                        'version': '1'
+                    },
+                    'inputArtifacts': [
+                        {
+                            'name': 'SourceOutput'
                         }
-                    }
-                ]
-            },
-            {
-                'name': 'Build',
-                'actions': [
-                    {
-                        'name': 'BuildAction',
-                        'actionTypeId': {
-                            'category': 'Build',
-                            'owner': 'AWS',
-                            'provider': 'CodeBuild',
-                            'version': '1'
-                        },
-                        'inputArtifacts': [
-                            {
-                                'name': 'SourceOutput'
-                            }
-                        ],
-                        'outputArtifacts': [
-                            {
-                                'name': 'BuildOutput'
-                            }
-                        ],
-                        'configuration': {
-                            'ProjectName': github-lambda-ecr
+                    ],
+                    'outputArtifacts': [
+                        {
+                            'name': 'BuildOutput'
                         }
-                    }
-                ]
-            },
-            {
-                'name': 'Deploy',
-                'actions': [
-                    {
-                        'name': 'DeployAction',
-                        'actionTypeId': {
-                            'category': 'Deploy',
-                            'owner': 'AWS',
-                            'provider': 'Lambda',
-                            'version': '1'
-                        },
-                        'inputArtifacts': [
-                            {
-                                'name': 'BuildOutput'
-                            }
-                        ],
-                        'configuration': {
-                            'FunctionName': github-to-lambda-demo
+                    ],
+                    'configuration': {
+                        'ProjectName': codebuild_project_name
+                    },
+                    'runOrder': 1
+                }
+            ]
+        },
+        {
+            'name': 'Deploy',
+            'actions': [
+                {
+                    'name': 'DeployAction',
+                    'actionTypeId': {
+                        'category': 'Deploy',
+                        'owner': 'AWS',
+                        'provider': 'Lambda',
+                        'version': '1'
+                    },
+                    'inputArtifacts': [
+                        {
+                            'name': 'BuildOutput'
                         }
-                    }
-                ]
-            }
-        ]
-    }
-)
+                    ],
+                    'configuration': {
+                        'FunctionName': lambda_function_name
+                    },
+                    'runOrder': 1
+                }
+            ]
+        }
+    ]
+}
+
+# Create or update the pipeline
+try:
+    response = codepipeline_client.get_pipeline(name=pipeline_name)
+    print(f"Pipeline {pipeline_name} exists. Updating...")
+    response = codepipeline_client.update_pipeline(pipeline=pipeline)
+except codepipeline_client.exceptions.PipelineNotFoundException:
+    print(f"Pipeline {pipeline_name} does not exist. Creating...")
+    response = codepipeline_client.create_pipeline(pipeline=pipeline)
 
 print(response)
